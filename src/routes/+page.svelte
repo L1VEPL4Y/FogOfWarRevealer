b056e94ad597a812807cbaab2c6aead53b9eba44
Clemens Reutin <clemens@reutin.at>
2025-09-01 15:33:14 +0200

    added basic file loading and displaying tech. will be improved later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,79 @@
 <script lang="ts">
-import { AppBar } from '@skeletonlabs/skeleton-svelte';
-import ArrowLeft from '@lucide/svelte/icons/arrow-left';
-import Paperclip from '@lucide/svelte/icons/paperclip';
-import Calendar from '@lucide/svelte/icons/calendar';
-import CircleUser from '@lucide/svelte/icons/circle-user';
+  import { AppBar } from '@skeletonlabs/skeleton-svelte';
+  import ImageUp from '@lucide/svelte/icons/image-up';
+  import Blend from '@lucide/svelte/icons/blend';
+  import Save from '@lucide/svelte/icons/save';
+  import Pen from '@lucide/svelte/icons/pen';
+  import Radius from '@lucide/svelte/icons/radius';
+  import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+  import IconBolt from '@lucide/svelte/icons/bolt';
+  import IconImage from '@lucide/svelte/icons/image';
+	import { read } from '$app/server';
+	
+  // State
+  let value = $state('files');
+  let loadedMapSrc = $state("");
+  let loadedMaskSrc = $state("");
+  // Logic
+  function openMapFilePicker(){
+    document.getElementById("mapFileInput")?.click();
+  }
+  function openMaskFilePicker(){
+    document.getElementById("maskFileInput")?.click();
+  }
+  function loadFile(isMap:boolean, event:Event){
+    if(event.target instanceof HTMLInputElement){
+      if(event.target.files && event.target.files[0]){
+        let reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onload = (loadEvent) => {
+          if(loadEvent.target == null) return;
+          if(loadEvent.target.result == null) return;
+          if(isMap) {
+            loadedMapSrc = loadEvent.target.result as string;
+          } else {
+            loadedMaskSrc = loadEvent.target.result as string;
+          }
+        }
+      }
+    }
+  }
 </script>
 
 
-<AppBar>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<AppBar padding="pl-30 pt-4 pb-4">
   {#snippet lead()}
-    <ArrowLeft size={24} />
+    <ImageUp size={32} onclick={openMapFilePicker} class="hover:preset-filled-surface-50-950 p-1"/>
+    <Blend size={32} onclick={openMaskFilePicker} class="hover:preset-filled-surface-50-950 p-1"/>
+    <Save size={32} class="hover:preset-filled-surface-50-950 p-1"/>
+    <Pen size={32} class="hover:preset-filled-surface-50-950 p-1"/>
+    <Radius size={32} class="hover:preset-filled-surface-50-950 p-1"/>
   {/snippet}
-  {#snippet trail()}
-    <Paperclip size={20} />
-    <Calendar size={20} />
-    <CircleUser size={20} />
-  {/snippet}
-</AppBar>
+</AppBar>	
+<div class="card border-surface-100-900 grid h-full w-full grid-cols-[auto_1fr] border-[1px] ">
+		<!-- Component -->
+		<Navigation.Rail {value} onValueChange={(newValue) => (value = newValue)} >
+			{#snippet tiles()}
+				<Navigation.Tile href="/" label="DM View"><IconBolt /></Navigation.Tile>
+				<Navigation.Tile href="/player" label="Player View"><IconImage /></Navigation.Tile>
+			{/snippet}
+		</Navigation.Rail>
+		<!-- Content -->
+    <input type="file" id="mapFileInput" accept="image/*" style="display: none;" onchange={(event) => loadFile(true, event)}>
+    <input type="file" id="maskFileInput" accept="image/*" style="display: none;" onchange={(event) => loadFile(false, event)}>
+		<div class="flex items-start justify-center">
+      <!-- ToDo: Display image in Canvas for future zoom and pan functionality -->
+      <!-- ToDo: Show image with hidden Areas Masked Out using a Mask -->
+			<div class="w-full">
+        <img alt="your map goes here when it is loaded" src={loadedMapSrc} />
+      </div>
+		</div>
+</div>
+
+
---
+++ src/routes/player/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Navigation } from '@skeletonlabs/skeleton-svelte';
+  import IconBolt from '@lucide/svelte/icons/bolt';
+  import IconImage from '@lucide/svelte/icons/image';
+	
+  // State
+  let value = $state('files');
+</script>
+<div class="card border-surface-100-900 grid h-full w-full grid-cols-[auto_1fr] border-[1px] ">
+		<!-- Component -->
+		<Navigation.Rail {value} onValueChange={(newValue) => (value = newValue)} >
+			{#snippet tiles()}
+				<Navigation.Tile href="/" label="DM View"><IconBolt /></Navigation.Tile>
+				<Navigation.Tile href="/player" label="Player View"><IconImage /></Navigation.Tile>
+			{/snippet}
+		</Navigation.Rail>
+		<!-- Content -->
+		<div class="flex items-start justify-center">
+		</div>
+</div>
